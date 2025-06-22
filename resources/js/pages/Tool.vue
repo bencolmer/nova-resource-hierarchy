1abe8476e4e737e86d54caaa0673fd45a7431365
Ben Colmer <ben-colmer@outlook.com>
2025-06-22 13:47:34 +0100

    WIP added create action

--- resources/js/pages/Tool.vue
+++ resources/js/pages/Tool.vue
@@ -4,9 +4,18 @@
 
     <Heading>{{ title }}</Heading>
 
-    <p class="nrh-intro-copy">
-      Lorem ipsum dolor, sit amet consectetur adipisicing elit. Fugiat iste esse dolor provident cupiditate, repellendus illum laudantium!
-    </p>
+    <div class="nrh-hierarchy-header">
+      <p class="nrh-intro-copy">
+        Lorem ipsum dolor, sit amet consectetur adipisicing elit. Fugiat iste esse dolor provident cupiditate, repellendus illum laudantium!
+      </p>
+
+      <Link
+        :href="createUrl"
+        class="nrh-create-btn"
+      >
+        Create
+      </Link>
+    </div>
 
     <Card class="nrh-hierarchy-list-card">
       <HierarchyList
@@ -34,23 +43,86 @@ export default {
       default: true
     }
   },
+
+  computed: {
+    createUrl() {
+      return this.$url(
+        `/resources/${this.resourceUriKey}/new`
+      );
+    }
+  }
 }
 </script>
 
-<style>
-.nrh-intro-copy {
+<style lang="scss">
+.nrh-hierarchy-header {
+  display: flex;
+  flex-direction: column;
+  justify-content: space-between;
+  align-items: start;
+  gap: 1rem;
   margin-top: 0.75rem;
-  line-height: 1.25;
-
 }
+
+.nrh-intro-copy {
+  line-height: 1.25;
+}
+
+.nrh-create-btn {
+  display: flex;
+  align-items: center;
+  flex-shrink: 0;
+  height: 2.25rem;
+  padding-left: 1rem;
+  padding-right: 1rem;
+  margin-top: auto;
+  margin-left: auto;
+  font-weight: bold;
+  border-radius: 0.25rem;
+  font-size: 0.875rem;
+  line-height: 1.25rem;
+  color: white;
+  background-color: rgba(var(--colors-primary-500));
+  --btn-shadow: 0 1px 3px 0 rgba(0,0,0,0.1),0 1px 2px -1px rgba(0,0,0,0.1);
+  box-shadow: 0 0 #000000,0 0 #000000, var(--btn-shadow);
+
+  &:focus {
+    outline: 2px solid transparent;
+    outline-offset: 2px;
+
+    box-shadow: 0 0 0 0px #ffffff, 0 0 0 3px rgba(var(--colors-primary-200)), var(--btn-shadow);
+  }
+
+  &:hover {
+    background-color: rgba(var(--colors-primary-400));
+  }
+
+  &:active {
+    background-color: rgba(var(--colors-primary-600));
+  }
+
+  &:is(.dark *) {
+    color: rgba(var(--colors-gray-800));
+
+    &:focus {
+      box-shadow: 0 0 0 0px #ffffff, 0 0 0 3px rgba(var(--colors-gray-600)), var(--btn-shadow);
+    }
+  }
+}
+
 .nrh-hierarchy-list-card {
   padding: 1rem;
-  margin-top: 2rem;
+  margin-top: 1.5rem;
   width: 100%;
   overflow-x: auto;
 }
 
 @media (width >= 48rem) {
+  .nrh-hierarchy-header {
+    flex-direction: row;
+    gap: 4rem;
+  }
+
   .nrh-hierarchy-list-card {
     padding: 1.5rem;
   }
